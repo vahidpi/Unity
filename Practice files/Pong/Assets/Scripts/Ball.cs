de7c940bae14b354340d8cfab831f0bfe8f79e35
Vahid <vpiroozbakht@gmail.com>
2018-03-14 00:15:37 +0200

    pong 2d

---
+++ Practice files/Pong/Assets/Scripts/Ball.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public float speed = 30;
+
+    void Start() {
+        // Initial Velocity
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+}
