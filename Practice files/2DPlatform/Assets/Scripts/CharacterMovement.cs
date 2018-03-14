92607508fc9117ab8921747edc27f7b6f077864f
Oppija <oppija@tite.amiedu-ict.net>
2018-03-14 14:50:58 +0200

    2D Platform

---
+++ Practice files/2DPlatform/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class CharacterMovement : MonoBehaviour {
+
+    public float movementSpeed;
+    public float jumpForce;
+
+    public Rigidbody2D rb2D;
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         * Write here code,
+         * where do you use a and d to move th player
+         * And space to jump.
+         * Rigidbody2D.addforce to jump player
+         * 10min
+         */
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            //animator.SetTrigger("Jump");
+            animator.SetBool("Walk", true);
+            transform.Translate(new Vector2(Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime, 0));
+        } else {
+            // we are standing
+            animator.SetBool("Walk", false);
+        }
+
+        // flip the player when going other directyion
+        if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("Jump");
+            rb2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+        //animator.parameters.SetValue("Walk", true);
+        //Input.GetAxis("Vertical");
+
+        /*
+         create 3 small level
+         create a small particle effect to the end of level
+         create an Ice cube tage as "Enemy" to some place in the level
+         create a "WorldMap" scene, where is Map.png and background
+         Create a "MapPlayer" use MapIcon.png
+         Player moves using w,a,s,d on map
+
+        Next Tims:
+        Dynamic health Bar for the Character
+        Scene change from map tp levels and back
+        Posiibly fix jump bug, sp player cannot jump in the air
+
+         */
+    }
+}
---
+++ Practice files/2DPlatformer/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CharacterMovement : MonoBehaviour {
+
+    public float movementSpeed;
+    public float jumpForce;
+
+    public Animator animator;
+
+    public Rigidbody2D rb2D;
+
+
+	// Use this for initialization
+	void Start () {
+
+        animator = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        /*
+		Write here some code where you use a and d to move the player.
+        And Space to jump. Use Rigidbody2D.addforce to jump player. 
+        10min
+        */
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+            // We are moving
+            animator.SetBool("Walk", true);
+            transform.Translate(new Vector2(Input.GetAxis("Horizontal") *
+                movementSpeed * Time.deltaTime, 0));
+        }
+        else
+        {
+            // We are standing still
+            animator.SetBool("Walk", false);
+        }
+
+        // Flip the player when going other direction
+        if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("Jump");
+            rb2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+
+        }
+        /*
+        Homework/Schoolwork
+        Create 3 different small!! levels (scenes)
+        Create a small particle effect to the end of each the level
+        Create an IceCube (tagged as "Enemy") to some place in the level
+        Create a "WorldMap" Scene, where is Map.png and background
+        Create a "MapPlayer" use MapIcon.png. 
+        Player moves using w,a,s,d on map
+
+        Next time we'll do following:
+        Dynamic Health Bar for the character
+        Scene change from map to levels and back to map
+        Possibly fix jump bug, so player cannot jump in the air
+
+
+        */
+
+    }
+}
